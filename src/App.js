085a7619d3549c6fb00e295399af9be587fcc34d
Dylan <dylan.cobham@yahoo.com.au>
2022-11-13 20:55:29 +1030

    build: add browser router navigation between pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
